639e1142713a3efca47a89aedfb9c775df61c49b
JorgeFigueroa-Iteso <jorge.figueroa@iteso.mx>
2023-04-19 20:01:56 -0600

    a

--- Entrega 2/src/Main.java
+++ Entrega 2/src/Main.java
@@ -1,5 +1,12 @@
+/**
+ * @author Jorge Ramón Figueroa Maya, Juan Pablo Álvarez Camarena
+ * @version 1.0
+ * <p>Proyecto: Magic The Gathering
+ * <p>El programa creará Mazos de cartas de Magic The Gathering
+ * <p>para jugar con ellos en el juego de mesa.
+ * */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
